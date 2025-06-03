25b934dbaece0c97958c6d750e97a3d1c5297d87
Thanh Hòa <thanhhoa159357123@gmail.com>
2025-06-04 01:38:51 +0700

    Add admin UI with React + Vite + Tailwind

---
+++ fe-admin/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='text-8xl text-cyan-500'>
+      Hello World!
+    </div>
+  )
+}
+
+export default App
